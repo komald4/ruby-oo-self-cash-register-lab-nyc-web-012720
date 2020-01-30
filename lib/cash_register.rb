a23957ffa022a00b75a9ebed3de1f5849015cfb3
Komal Dindiyal <komald.4@gmail.com>
2020-01-30 22:20:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,29 @@
 require 'pry'
 class CashRegister
-
-  attr_accessor :total, :employee_discount
+  attr_accessor :total, :employee_discount, :items
 
   def initialize (employee_discount = nil)
     @total = 0.00
     @employee_discount = employee_discount
+    @items = []
   end
-
-
-
-  def add_item(title, price)
-
+  def discount
+    self.employee_discount
   end
-
-end
+  def items
+    @items
+  end
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times do
+      items << title
+    end
+  end
+  def apply_discount
+    if @employee_discount
+      @total = @total * (1 - @employee_discount / 100)
+      "After the discount, the total comes to $#{@total}"
+    else
+      "There is no discount to apply."
+    end
+  end
