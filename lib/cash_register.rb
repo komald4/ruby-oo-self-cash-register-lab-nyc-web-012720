15128c479e2ead727d0ae0482079899f691cc570
Komal Dindiyal <komald.4@gmail.com>
2020-01-30 22:18:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,9 +4,9 @@ class CashRegister
   attr_accessor :total, :employee_discount
 
 
-  def initialize
-      @total = 0.00
-  end
+  # def initialize
+  #     @total = 0.00
+  # end
 
   def discount(discount = 20)
     @total = discount
